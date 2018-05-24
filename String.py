e4bab20a0fb373539fbe455b8bb6d69d39f3cc13
krta2 <krta22@gmail.com>
2018-05-24 20:45:47 +0900

    done

---
+++ String.py
@@ -0,0 +1,28 @@
+print('Hello')
+print("Hello")
+print("Hello 'world'")
+print('Hello "world"')
+
+print('Hello '+'world')
+print('Hello '*3)
+print('Hello'[0])
+print('Hello'[1])
+print('Hello'[2])
+
+# functions
+print('hello world'.capitalize())
+print('hello world'.upper())
+print('hello world'.__len__())
+print(len('hello world'))
+print('Hello world'.replace("world", 'programming'))
+
+# special chars
+print("egoing's \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\t\tworld")
+print("\a") #alert
+print('Hello\nworld')
+
+print(10+5)
+print("10"+"5")
